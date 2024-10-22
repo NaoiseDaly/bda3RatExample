79ea276bba5933fbbc9fcf02f14d4e047345df14
Naoise <naoisedaly101@gmail.com>
2024-10-22 13:03:16 +0100

    fixed a x instead of + in messy_part_in_posterior_log small tidying of code added basic comments

--- at1.py
+++ at1.py
@@ -1,35 +1,8 @@
-import pandas as pd
 from scipy.special import loggamma
 import numpy as np
-import matplotlib.pyplot as plt
 
-dataset = pd.read_csv("tumour-data.txt", names = ("y.j", "n.j"))
-
-
-def some_diffuse_prior_log(alpha, beta):
-    return np.log(alpha+beta)*(-5/2)
-
-def messy_part_in_posterior_log(alpha, beta, d):
-    parts = [None for _ in range(d.shape[0])]
-    for j in range(d.shape[0]):
-        top = loggamma(alpha+beta)+loggamma(alpha+d.loc[j, "y.j"])+loggamma(beta+d.loc[j, "n.j"]-d.loc[j, "y.j"])
-        bottom = loggamma(alpha)*loggamma(beta)*loggamma(alpha+beta+d.loc[j, "n.j"])
-        parts[j] = top-bottom #log scale
-    
-    return sum(parts)
-
-def unnormalised_posterior(alpha, beta, prior, data):
-    mess = messy_part_in_posterior_log(alpha, beta, data)
-    print(mess)
-    print(np.exp(mess))
-    # return prior(alpha, beta)*
-    return mess+prior(alpha, beta)
-
-
-
-
-
-
+"""Helper functions
+"""
 def transform_to_alpha_beta(logit_mean, log_sample_size):
     beta = np.exp(log_sample_size)/(1+np.exp(logit_mean))
     alpha = np.exp(logit_mean)*beta
@@ -41,14 +14,30 @@ def transform_from_alpha_beta(alpha, beta):
     return logit_mean, log_sample_size
 
 
+"""Different pdfs or parts of them
+"""
+
+def some_diffuse_prior_log(alpha, beta):
+    """The log of a suggested prior."""
+    return np.log(alpha+beta)*(-5/2)
+
+def messy_part_in_posterior_log(alpha, beta, d):
+    """The log of the 'non-prior' part of the posterior"""
+    parts = [None for _ in range(d.shape[0])]
+    for j in range(d.shape[0]):
+        top = loggamma(alpha+beta)+loggamma(alpha+d.loc[j, "y.j"])+loggamma(beta+d.loc[j, "n.j"]-d.loc[j, "y.j"])
+        bottom = loggamma(alpha)+loggamma(beta)+loggamma(alpha+beta+d.loc[j, "n.j"])
+        parts[j] = top-bottom 
+    
+    return sum(parts)
+
+def unnormalised_posterior(alpha, beta, prior, data):
+    mess = messy_part_in_posterior_log(alpha, beta, data)
+    print(mess)
+    print(np.exp(mess))
+    return mess+prior(alpha, beta)
+
 
 GRID = {"logit_mean":np.linspace(-2.3,-1.3, num = 100)
      ,"log_sample_size":np.linspace(1,5, num = 100)
-     ,"posterior": np.zeros((100,2))
      }   
-
-# alpha_on_grid, beta_on_grid 
-# alpha_on_grid_mesh, beta_on_grid_mesh = np.meshgrid(alpha_on_grid, beta_on_grid)#make them into co-ord matrices
-# posterior_on_grid = unnormalised_posterior(alpha_on_grid,beta_on_grid
-#                                            ,some_diffuse_prior, dataset)
-
--- debug.py
+++ debug.py
@@ -1,18 +1,21 @@
 from at1 import *
 import pandas as pd
+import matplotlib.pyplot as plt
+
 dataset = pd.read_csv("tumour-data.txt", names = ("y.j", "n.j"))
 
 alpha_on_grid, beta_on_grid = transform_to_alpha_beta(GRID["logit_mean"], GRID["log_sample_size"])
 alpha_on_grid_mesh, beta_on_grid_mesh = np.meshgrid(alpha_on_grid, beta_on_grid)
 
 
-out = unnormalised_posterior(alpha_on_grid_mesh,beta_on_grid_mesh,some_diffuse_prior_log,dataset)
+output = unnormalised_posterior(
+    alpha_on_grid_mesh,beta_on_grid_mesh
+    ,some_diffuse_prior_log,dataset)
 
-GRID["posterior"] = out 
-print()
-print(out)
+GRID["posterior"] = output 
+
 plt.contourf(
-        GRID["logit_mean"], GRID["log_sample_size"], out
+        GRID["logit_mean"], GRID["log_sample_size"], GRID["posterior"]
     )
 
 plt.colorbar()
