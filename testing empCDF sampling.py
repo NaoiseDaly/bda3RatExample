1762e0e02b209d9922dd186d6f0cda86635e50aa
ND <121348631@umail.ucc.ie>
2024-10-24 17:59:34 +0100

    further refining testing empCDF about to refactor to allow pulling value direct rather than the index of the sampled value

--- testing empCDF sampling.py
+++ testing empCDF sampling.py
@@ -1,16 +1,15 @@
 from at1 import *
-import pandas as pd
 import matplotlib.pyplot as plt
 
 vals = np.array(range(1,5+1))
 dist = np.array([.1,.3,.3,.1,.2])
-ind_draws = [sample_from_empirical_dist(dist) for _ in range(20)]
-print(vals[ind_draws][:20])
+ind_draws = [sample_from_empirical_dist(dist) for _ in range(20000)]
+print(np.unique(vals[ind_draws], return_counts=True))
 
 
 fig, (ax1, ax2) = plt.subplots(1,2)
 ax1.bar(vals, dist)
 ax1.set_title("theoretic")
-ax2.hist(ind_draws)
+ax2.bar(*np.unique(vals[ind_draws], return_counts=True))
 ax2.set_title("simulated")
 plt.show()
