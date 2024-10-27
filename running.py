a97c541e510fd10f10a97fc9c614690d0870c4ae
Naoise <naoisedaly101@gmail.com>
2024-10-27 19:40:58 +0000

    graphical comparison of sampling dist vs likelihood of alpha

--- running.py
+++ running.py
@@ -5,8 +5,8 @@ import matplotlib.pyplot as plt
 dataset = pd.read_csv("tumour-data.txt", names = ("y.j", "n.j"))
 
 
-GRID = {"logit_mean":np.linspace(-2.3,-1.3, num = 1000)
-     ,"log_sample_size":np.linspace(1,5, num = 1000)
+GRID = {"logit_mean":np.linspace(-2.3,-1.3, num = 100)
+     ,"log_sample_size":np.linspace(1,5, num = 100)
      } 
 GRID["alpha"], GRID["beta"] = transform_to_alpha_beta(GRID["logit_mean"], GRID["log_sample_size"])
 
@@ -27,10 +27,7 @@ plot1 = ax1.contourf(
     )
 ax1.set_xlabel("alpha")
 ax1.set_ylabel("beta")
-# cax = divider.append_axes('right', size='5%', pad=0.05)
 plt.colorbar(plot1, ax =ax1)
-
-
 
 plot2 = ax2.contourf(
         GRID["logit_mean"], GRID["log_sample_size"], GRID["posterior"]
@@ -40,9 +37,17 @@ ax2.set_ylabel("log_sample_size")
 plt.colorbar(plot2, ax =ax2)
 plt.show()
 
-# posterior_alpha = marginal_of_alpha_on_grid(GRID["posterior"])
-# plt.bar(GRID["alpha"], posterior_alpha)
-# print(sum(posterior_alpha))
-# plt.show()
+s = 20000 #number of simulated samples 
+posterior_alpha = marginal_of_alpha_on_grid(GRID["posterior"])
+draws_of_alpha = sample_from_empirical_dist(s, GRID["alpha"], posterior_alpha)
+
+fig, (ax1, ax2) = plt.subplots(1,2, sharex = True)
+ax1.bar(GRID["alpha"], posterior_alpha)
+ax1.set_title("marginal unnormalised posterior of alpha")
+values, heights = np.unique(draws_of_alpha, return_counts=True)
+ax2.bar(values, heights)
+ax2.set_title(f"frequency of {s} simulated draws from posterior")
+plt.show()
+
 
 
