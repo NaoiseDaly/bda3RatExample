76bcd6b58defbbc6c12248e13a5a43f854ee2826
Naoise <naoisedaly101@gmail.com>
2024-10-29 09:14:22 +0000

    refactored GRID due to non-uniform spacing on alpha,beta scale added jitter to sampled points.

--- running.py
+++ running.py
@@ -1,14 +1,21 @@
 from at1 import *
 import pandas as pd
 import matplotlib.pyplot as plt
+from scipy.stats import uniform
 
 dataset = pd.read_csv("tumour-data.txt", names = ("y.j", "n.j"))
 
+SIZE_OF_GRID = 100
+#equal spacing on the transformed scale does not mean equal spacing on the original scale
+alpha_boundaries, beta_boundaries = transform_to_alpha_beta((-2.3,-1.3), (1, 5))
+STEP_SIZE_IN_ALPHA = (alpha_boundaries[1]-alpha_boundaries[0])/SIZE_OF_GRID
+STEP_SIZE_IN_BETA = (beta_boundaries[1]-beta_boundaries[0])/SIZE_OF_GRID
 
-GRID = {"logit_mean":np.linspace(-2.3,-1.3, num = 100)
-     ,"log_sample_size":np.linspace(1,5, num = 100)
-     } 
-GRID["alpha"], GRID["beta"] = transform_to_alpha_beta(GRID["logit_mean"], GRID["log_sample_size"])
+GRID ={
+    "alpha": np.linspace(*alpha_boundaries, num = SIZE_OF_GRID),
+    "beta" : np.linspace(*beta_boundaries, num = SIZE_OF_GRID)
+}
+GRID["logit_mean"], GRID["log_sample_size"] = transform_from_alpha_beta(GRID["alpha"], GRID["beta"])
 
 alpha_on_grid_mesh, beta_on_grid_mesh = np.meshgrid(GRID["alpha"], GRID["beta"], sparse = True)
 
@@ -37,17 +44,9 @@ ax2.set_ylabel("log_sample_size")
 plt.colorbar(plot2, ax =ax2)
 plt.show()
 
-s = 20000 #number of simulated samples 
+s = 2000 #number of simulated samples 
 posterior_alpha = marginal_of_alpha_on_grid(GRID["posterior"])
 draws_of_alpha = sample_from_empirical_dist(s, GRID["alpha"], posterior_alpha)
-
-fig, (ax1, ax2) = plt.subplots(1,2, sharex = True)
-ax1.bar(GRID["alpha"], posterior_alpha)
-ax1.set_title("marginal unnormalised posterior of alpha")
-values, heights = np.unique(draws_of_alpha, return_counts=True)
-ax2.bar(values, heights)
-ax2.set_title(f"frequency of {s} simulated draws from posterior")
-plt.show()
 
 draws_of_beta_given_alpha = np.array([None for _ in range(s)], dtype = float)
 
@@ -55,6 +54,11 @@ for i, alpha_sample in enumerate(draws_of_alpha):
     beta_dist = beta_on_grid_given_alpha(GRID, alpha_sample)
     beta_sample = sample_from_empirical_dist(1, GRID["beta"], beta_dist )[0]
     draws_of_beta_given_alpha[i] = beta_sample
+
+#add jitter to sampled pairs
+draws_of_alpha += uniform.rvs(size = s, scale = STEP_SIZE_IN_ALPHA)
+draws_of_beta_given_alpha += uniform.rvs(size = s, scale = STEP_SIZE_IN_BETA)
+
 
 fig, (ax1, ax2) = plt.subplots(1,2)
 
@@ -65,4 +69,12 @@ ax1.set_ylabel("beta")
 ax2.scatter(*transform_from_alpha_beta(draws_of_alpha, draws_of_beta_given_alpha))
 ax2.set_xlabel("logit_mean")
 ax2.set_ylabel("log_sample_size")
+plt.show()
+
+fig, (ax1, ax2) = plt.subplots(1,2, sharex = True)
+ax1.bar(GRID["alpha"], posterior_alpha)
+ax1.set_title("marginal unnormalised posterior of alpha")
+values, heights = np.unique(draws_of_alpha, return_counts=True)
+ax2.bar(values, heights)
+ax2.set_title(f"frequency of {s} simulated draws from posterior")
 plt.show()
